4a236a5052d29d5a152adab806d5520c8f94b46b
Julia Coutinho <julia.coutinho.ribeiro@gmail.com>
2026-03-25 18:57:48 +0000

    atualização de vizualização

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import datetime
-import os
-import barcode
-from barcode.writer import ImageWriter
 import pandas as pd
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'dev'
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
 
 class Caixa(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     codigo = db.Column(db.String(50), unique=True)
     vendedor = db.Column(db.String(50))
     data = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    itens = db.relationship('Item', backref='caixa', lazy=True)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,40 +27,31 @@ class Item(db.Model):
     recebidas = db.Column(db.Integer)
     processadas = db.Column(db.Integer)
     divergencia = db.Column(db.Integer)
+
     caixa_id = db.Column(db.Integer, db.ForeignKey('caixa.id'))
 
-class Conferencia(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    caixa_id = db.Column(db.Integer)
-    conferido_por = db.Column(db.String(50))
-    data = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    observacao = db.Column(db.Text)
-
-
-def gerar_codigo(prefixo):
-    return f"{prefixo}-{int(datetime.datetime.now().timestamp())}"
-
-def gerar_barcode(codigo):
-    os.makedirs("static/barcodes", exist_ok=True)
-    path = f"static/barcodes/{codigo}"
-    CODE128 = barcode.get_barcode_class('code128')
-    CODE128(codigo, writer=ImageWriter()).save(path)
-    return f"barcodes/{codigo}.png"
 
 
 @app.route('/')
 def index():
-    caixas = Caixa.query.all()
+    busca = request.args.get('busca')
+
+    if busca:
+        caixas = Caixa.query.filter(
+            (Caixa.codigo.contains(busca)) |
+            (Caixa.vendedor.contains(busca))
+        ).all()
+    else:
+        caixas = Caixa.query.all()
+
     return render_template('index.html', caixas=caixas)
+
 
 @app.route('/nova_caixa', methods=['GET', 'POST'])
 def nova_caixa():
     if request.method == 'POST':
-        codigo = gerar_codigo("CX")
-        gerar_barcode(codigo)
-
         caixa = Caixa(
-            codigo=codigo,
+            codigo=f"CX-{int(datetime.datetime.now().timestamp())}",
             vendedor=request.form['vendedor']
         )
         db.session.add(caixa)
@@ -69,11 +60,6 @@ def nova_caixa():
 
     return render_template('criar_caixa.html')
 
-@app.route('/caixa/<int:id>')
-def detalhe_caixa(id):
-    caixa = Caixa.query.get_or_404(id)
-    itens = Item.query.filter_by(caixa_id=id).all()
-    return render_template('detalhe_caixa.html', caixa=caixa, itens=itens)
 
 @app.route('/item/<int:caixa_id>', methods=['GET', 'POST'])
 def criar_item(caixa_id):
@@ -90,37 +76,13 @@ def criar_item(caixa_id):
         )
         db.session.add(item)
         db.session.commit()
-        return redirect(url_for('detalhe_caixa', id=caixa_id))
+        return redirect(url_for('index'))
 
     return render_template('criar_item.html', caixa_id=caixa_id)
 
-@app.route('/buscar', methods=['GET', 'POST'])
-def buscar():
-    caixa = None
-    if request.method == 'POST':
-        codigo = request.form['codigo']
-        caixa = Caixa.query.filter_by(codigo=codigo).first()
-    return render_template('buscar_caixa.html', caixa=caixa)
-
-@app.route('/conferencia/<int:id>', methods=['GET', 'POST'])
-def conferencia(id):
-    caixa = Caixa.query.get_or_404(id)
-
-    if request.method == 'POST':
-        conf = Conferencia(
-            caixa_id=id,
-            conferido_por=request.form['nome'],
-            observacao=request.form['obs']
-        )
-        db.session.add(conf)
-        db.session.commit()
-
-    return render_template('conferencia.html', caixa=caixa)
 
 
 def importar_excel():
-    import pandas as pd
-
     df = pd.read_excel('dados.xlsx')
 
     grupos = df.groupby('Vendedor')
@@ -151,11 +113,11 @@ def importar_excel():
     db.session.commit()
 
 
+
 if __name__ == '__main__':
     with app.app_context():
-        ## db.drop_all()
+        db.drop_all()
         db.create_all()
-        ## importar_excel()
-        
+        importar_excel() 
 
     app.run(debug=True)
